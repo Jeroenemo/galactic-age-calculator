48064f892a7a82f66ced16ecbb86eddb5f49d9ab
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-07 18:02:25 -0800

    write spec for mercury age

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -10,4 +10,7 @@ describe('Calculator', () => {
   test('should create player object with the passed in argument and fixed values', () => {
     expect(calculator).toEqual({age: 30, lifeSpan: 95, planetYears: {'Mercury': 0.24, 'Venus': 0.62, 'Earth': 1, 'Mars': 1.88, 'Jupiter': 11.86}});
   });
+  test('should return user age in Mercury years', () => {
+    expect(calculator.ageOnPlanet('mercury')).toEqual();
+  });
 });
