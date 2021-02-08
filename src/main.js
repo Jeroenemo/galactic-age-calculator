9358a803ff7fded4c55aaefdcf23ed27f3def14a
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-07 19:53:33 -0800

    read in form value and add event listener

--- src/main.js
+++ src/main.js
@@ -2,3 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Calculator from './calculator.js';
+
+$(document).ready(function() {
+  $('form#user-info').submit(function(event) {
+    event.preventDefault();
+  });
+  $('button#submit').on('click', function() {
+    $('#monkey').fadeOut('slow');
+    const name = $('#name').val();
+    const age = parseInt($('#age').val());
+    const diet = $('input:radio[name=diet]:checked').val();
+    const exercise = $('input:radio[name=exercise]:checked').val();
+    const seatbelt = $('input:radio[name=seatbelt]:checked').val();
+    const smoke = $('input:radio[name=smoke]:checked').val();
+    const drink = $('input:radio[name=drink]:checked').val();
+    let calculator = new Calculator(name, age);
+    calculator.lifeExpectancy(diet, exercise, seatbelt, smoke, drink);
+  });
+});
