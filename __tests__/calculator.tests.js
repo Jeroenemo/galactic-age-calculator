bb71953e47b3a3c8e22a3239614a23b317c446b9
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-07 17:55:00 -0800

    write spec for constructor

---
+++ __tests__/calculator.tests.js
@@ -0,0 +1,11 @@
+describe('Calculator', () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new Calculator(30);
+  });
+
+  test('should create player object with passed in arguments', () => {
+    expect(calculator).toEqual({age: 30, lifeSpan: 95, planetYears: {'mercury': 0.24, 'venus': 0.62, 'earth': 1, 'mars': 1.88, 'jupiter': 11.86}});
+  });
+});
