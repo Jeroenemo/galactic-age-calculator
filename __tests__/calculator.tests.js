894f6b5944cc108fe955c45d1215d9bb45d52012
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-07 19:07:57 -0800

    red test for years past jupiter

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -76,4 +76,8 @@ describe('Calculator', () => {
     const calculator = new Calculator(100);
     expect(calculator.yearsLeft("Mars")).toEqual("You have surpassed your life expectancy by 3 years!");
   });
+  test('should return years lived past life expectancy if planet is earth', () => {
+    const calculator = new Calculator(100);
+    expect(calculator.yearsLeft("Jupiter")).toEqual("You have surpassed your life expectancy by 3 years!");
+  });
 });
