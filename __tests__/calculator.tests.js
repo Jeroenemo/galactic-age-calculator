9d50ab9606bfe0583fc9590cffa6d506ad1eb09f
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-07 18:50:35 -0800

    green test years left venus

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -49,6 +49,6 @@ describe('Calculator', () => {
     expect(calculator.yearsLeft("Mercury")).toEqual("You have 270 years to live!");
   });
   test('should return years left if planet is venus', () => {
-    expect(calculator.yearsLeft("venus")).toEqual("You have 104 years to live!");
+    expect(calculator.yearsLeft("Venus")).toEqual("You have 104 years to live!");
   });
 });
