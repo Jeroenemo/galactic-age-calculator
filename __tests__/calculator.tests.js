62c1e081f61a44cb3501e5dc89e458a1aac9fa6e
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-07 17:57:06 -0800

    refactor spec

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -5,7 +5,7 @@ describe('Calculator', () => {
     calculator = new Calculator(30);
   });
 
-  test('should create player object with passed in arguments', () => {
+  test('should create player object with the passed in argument and fixed values', () => {
     expect(calculator).toEqual({age: 30, lifeSpan: 95, planetYears: {'mercury': 0.24, 'venus': 0.62, 'earth': 1, 'mars': 1.88, 'jupiter': 11.86}});
   });
 });
